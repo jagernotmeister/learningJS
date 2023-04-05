aa3f58eacbc6323cd5795843d7f8fef04fba1107
jager <jaredguardia00@gmail.com>
2023-04-05 17:01:03 -0300

    archivos

---
+++ myJSDoc.js
@@ -0,0 +1,26 @@
+console.log es la funcion que me permite ver logear en la consola lo que necesito
+console.error es para marcar un error dentro de la hoja de codigo
+console.warn es para emitir un warning dentrop de la consola
+
+console tambien tiene mas funciones, se pueden ver en mdn 
+
+VARIABLES LET Y CONST
+
+se usan las variables let y construction
+con let se pueden reasuignar valores en cambion con const no se pueden 
+const es por constante, no se puede cambiar
+
+let score = 20
+score = 10 
+se puede reasignar una nuevo valores en cambion
+
+DATA TYPES
+
+strings: const name = "john"; sirve para asignar un texto con o sin numeros a una variable se seniala con comillas
+numbers: const age = 23; sirve para asignar numeros a una variable, sin nada de por Medio, solo el numero
+boolean const isCool = true sirve para asignar a la variable un valor verdadero o falso
+null: const x = null sirve apra asignarla a la avariable un valor vacio
+undefined: const y = undefined sirve para agregarle a la variable un valor indefinido
+
+
+
